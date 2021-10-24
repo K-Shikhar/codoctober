343af1d967afeedfe000ecf00b951f46d86de213
Abhishek Bharadwaz <abhishekbharadwaz22@gmail.com>
2021-10-24 13:01:46 +0530

    notes section added

--- src/App.js
+++ src/App.js
@@ -1,14 +1,37 @@
 import './App.css';
-import Login from './containers/Login.js'
+import { BrowserRouter, Switch, Route, Redirect } from 'react-router-dom';
 import { Provider } from 'react-redux';
+import { URL_PREFIX } from './constant';
+import Login from './containers/Login';
+import Notes from './containers/Notes';
 import store from './redux/store';
 
 function App() {
     return (
         <Provider store={store}>
-            <div className="App">
-                <Login />
-            </div>
+            <BrowserRouter>
+                <Switch>
+                    <Route
+                        exact
+                        path={URL_PREFIX + '/login'}
+                        component={Login}
+                        key='route-login-screen'
+                    />
+                    <Route
+                        exact
+                        path={URL_PREFIX + '/signup'}
+                        component={Login}
+                        key='route-signup-screen'
+                    />
+                    <Route
+                        exact
+                        path={URL_PREFIX + '/user/notes'}
+                        component={Notes}
+                        key='route-notes-screen'
+                    />
+                    <Redirect to={URL_PREFIX + '/home'} />
+                </Switch>
+            </BrowserRouter>
         </Provider>
     );
 }
---
+++ src/containers/Notes.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import NotesComponent from '../components/NotesApp/Notes'
+
+class Notes extends Component {
+    render() {
+        return (
+            <div>
+                <NotesComponent />
+            </div>
+        )
+    }
+}
+
+export default Notes;
