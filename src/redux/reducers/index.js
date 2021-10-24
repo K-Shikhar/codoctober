04c3bd76f97caf0a8ad767810517ece17444f95f
Abhishek Bharadwaz <abhishekbharadwaz22@gmail.com>
2021-10-25 01:41:21 +0530

    feed redux added

--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from "redux";
 import auth from "./auth";
-import notes from './notes'
+import notes from './notes';
+import feed from './feed';
 
 export default combineReducers({
     authReducer: auth,
     notesReducer: notes,
+    feedReducer: feed,
 });
