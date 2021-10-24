e7673fcdf0f3ee725904637d2b099467e7f9a0a4
Abhishek Bharadwaz <55627658+4Marsha1@users.noreply.github.com>
2021-10-24 11:35:32 +0530

    login UI and redux (#1)
    * login UI and redux
    
    * minor changes
    
    * minor changes

--- src/containers/Login.js
+++ src/containers/Login.js
@@ -1,20 +1,51 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { loginUser } from '../redux/actions/auth'
 import LoginComponent from '../components/LoginPage'
 
 class Login extends Component {
     constructor(props) {
-        super(props)
-
+        super(props);
         this.state = {
+            isGettingSubmitted: false,
+            email_login: props.auth.lastUsedEmail || "",
+            password_login: "",
+        };
+    }
 
+    handleChange = (event) => {
+        this.setState({ [event.target.name]: event.target.value });
+    };
+
+    handleFormSubmit = (event) => {
+        event.preventDefault();
+        this.props.dispatch(loginUser(this.state.email_login, this.state.password_login));
+    };
+
+    componentDidUpdate() {
+        if (this.props.auth.isAuthenticated) {
+            this.props.history.replace('....Dashboard Redirect');
         }
     }
 
     render() {
-        return <div>
-            <LoginComponent />
-        </div>;
+        const { email_login, password_login } = this.state;
+        const { loginInitiated } = this.props.auth;
+        return (
+            <LoginComponent
+                email_login={email_login}
+                password_login={password_login}
+                isGettingSubmitted={loginInitiated}
+                handleChange={this.handleChange}
+                handleFormSubmit={this.handleFormSubmit}
+            />
+        );
     }
 }
 
-export default Login;
+
+const mapStateToProps = state => ({
+    auth: state.authReducer
+});
+
+export default connect(mapStateToProps)(Login);
